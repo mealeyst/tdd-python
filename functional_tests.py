84ca4ddf9724f16669e424736be90abf249e04de
Shawn Mealey <smealey@northpointdigital.com>
2015-05-08 14:17:36 -0400

    Adding redirect after POST, showing all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -20,7 +20,6 @@ class NewVisitorTest(unittest.TestCase):
     # Edith has heard about a cool new online to-do app. She goes
     # to check out its homepage 
     self.browser.get(' http://localhost:8000')
-
     # She notices the page title and header mention to-do lists 
     self.assertIn('To-Do', self.browser.title)
     header_text = self.browser.find_element_by_tag_name('h1').text
@@ -46,7 +45,7 @@ class NewVisitorTest(unittest.TestCase):
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
     inputbox = self.browser.find_element_by_id('id_new_item')
-    inputbox.send_keys('Use peacock features to make a fly')
+    inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
 
     # The page updates again, and now shows both items on her list
