57a50c50a777e39dba11e076eaafc6e0a53f1ac4
Shawn Mealey <smealey@northpointdigital.com>
2015-05-13 15:59:09 -0400

    Add - Bootstrap theme to app, static file server to tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
   def setUp(self):
     self.browser = webdriver.Firefox()
     self.browser.implicitly_wait(3)
@@ -109,5 +109,3 @@ class NewVisitorTest(LiveServerTestCase):
       512,
       delta=5
     )
-
-    self.fail('Finish the test!')
